0620554f02651eaaea9fc2ba461a17a5ee7b321c
Zeb Pirzada <zpirzada@gmail.com>
2022-12-28 01:39:27 +0100

    Test again

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int x=5;
+        int y=7;
+        int z = x + y;
         System.out.println("Hello world!");
+        System.out.println("x + y = " + z);
     }
 }
